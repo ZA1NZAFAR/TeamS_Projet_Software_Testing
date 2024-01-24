79836bc46fa9bb02fa59c90b5fed043d58a0024a
Senhao JIANG <ben659@hotmail.com>
2024-01-24 17:16:30 +0100

    Added employeeGetsPromotedToManager

--- src/test/java/fr/efrei/playwright/PlaywrightTeamSApplicationTests.java
+++ src/test/java/fr/efrei/playwright/PlaywrightTeamSApplicationTests.java
@@ -86,7 +86,19 @@ class PlaywrightTeamSApplicationTests {
         }
     }
 
+    @Test
+    public void employeeGetsPromotedToManager() {
+        try (Playwright playwright = Playwright.create()) {
 
+            page.navigate("https://s.hr.dmerej.info/employees");
+            assertThat(page.getByRole(AriaRole.TABLE)).containsText("no");
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Edit")).nth(1).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Promote as manager")).click();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Proceed")).click();
+            assertThat(page.getByRole(AriaRole.TABLE)).containsText("yes");
+
+        }
+    }
 
 
 
