ec707d8b2ccd6ef7e76599a138d4203071916b8a
QV NGUYEN <qv.nguyen00@gmail.com>
2024-01-24 15:40:34 +0100

    init maven project

---
+++ src/main/java/fr/efrei/playwright/PlaywrightTeamSApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.playwright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywrightTeamSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaywrightTeamSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/efrei/playwright/PlaywrightTeamSApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.playwright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaywrightTeamSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
