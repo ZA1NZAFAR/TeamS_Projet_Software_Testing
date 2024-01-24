80d71757d0138699f4fc03b012f7d9a82371bff2
Rimberse <kerimpltm@gmail.com>
2024-01-24 16:30:37 +0100

    Organize better test resources, use global objects to better structure tests

--- src/test/java/fr/efrei/playwright/PlaywrightTeamSApplicationTests.java
+++ src/test/java/fr/efrei/playwright/PlaywrightTeamSApplicationTests.java
@@ -2,33 +2,53 @@ package fr.efrei.playwright;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @SpringBootTest
 class PlaywrightTeamSApplicationTests {
+    // Shared between all tests in this class
+    static Playwright playwright;
+    static Browser browser;
 
-    @Test
-    void contextLoads() {
+    // New instance for each test method
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
     }
 
     @Test
     public void ensureElementsAreVisible() {
-        try (Playwright playwright = Playwright.create()) {
-            BrowserType chromium = playwright.chromium();
-            Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
-            BrowserContext context = browser.newContext();
-            Page page = browser.newPage();
-            page.navigate("https://s.hr.dmerej.info");
+        // Indicates URL of the website being tested
+        page.navigate("https://s.hr.dmerej.info");
 
-            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List Employees"))).isVisible();
-            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add new employee"))).isVisible();
-            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List teams"))).isVisible();
-            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Create new team"))).isVisible();
-            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Reset database"))).isVisible();
-            browser.close();
-        }
+        // Test steps
+        assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List Employees"))).isVisible();
+        assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add new employee"))).isVisible();
+        assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List teams"))).isVisible();
+        assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Create new team"))).isVisible();
+        assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Reset database"))).isVisible();
     }
 }
