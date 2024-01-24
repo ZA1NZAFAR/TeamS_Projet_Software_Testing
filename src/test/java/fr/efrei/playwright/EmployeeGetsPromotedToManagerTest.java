55592cdd4f196c13e537bbb06690f6e3b6b12716
Senhao JIANG <ben659@hotmail.com>
2024-01-24 18:24:25 +0100

    Refactored ManagerCanBePromotedToManager

--- src/test/java/fr/efrei/playwright/EmployeeGetsPromotedToManagerTest.java
+++ src/test/java/fr/efrei/playwright/EmployeeGetsPromotedToManagerTest.java
@@ -3,6 +3,7 @@ package fr.efrei.playwright;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
+import fr.efrei.playwright.utils.AddEmployeePageUtils;
 import fr.efrei.playwright.utils.HomepageUtils;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +15,7 @@ public class EmployeeGetsPromotedToManagerTest extends PlaywrightTeamSApplicatio
         try (Playwright playwright = Playwright.create()) {
             new HomepageUtils(page).goToEmployeesPage();
             assertThat(page.getByRole(AriaRole.TABLE)).containsText("no");
-            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Edit")).nth(1).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Edit")).first().click();
             page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Promote as manager")).click();
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Proceed")).click();
             assertThat(page.getByRole(AriaRole.TABLE)).containsText("yes");
--- src/test/java/fr/efrei/playwright/ManagerCanBePromotedToManagerTest.java
+++ src/test/java/fr/efrei/playwright/ManagerCanBePromotedToManagerTest.java
@@ -3,22 +3,21 @@ package fr.efrei.playwright;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
+import fr.efrei.playwright.utils.AddEmployeePageUtils;
 import fr.efrei.playwright.utils.HomepageUtils;
 import org.junit.jupiter.api.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-public class ManagerCannotBePromotedToManagerTest extends PlaywrightTeamSApplicationTests {
+public class ManagerCanBePromotedToManagerTest extends PlaywrightTeamSApplicationTests {
     @Test
-    public void managerCannotBePromotedToManager() {
+    public void managerCanBePromotedToManager() {
         try (Playwright playwright = Playwright.create()) {
-
             new HomepageUtils(page).goToHomepage();
+            new AddEmployeePageUtils(page).addManager();
             page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List Employees")).click();
-            assertThat(page.getByRole(AriaRole.TABLE)).containsText("yes");
             page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Edit")).first().click();
-            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Promote as manager"))).isHidden();
-
+            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Promote as manager"))).isVisible();
         }
     }
 }
