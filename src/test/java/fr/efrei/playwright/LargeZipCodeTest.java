c66b28e86c292b79d3d1562dcff3f52d66abaeee
za1nzafar <zainzafar@outlook.fr>
2024-01-24 17:50:51 +0100

    Added new test

---
+++ src/test/java/fr/efrei/playwright/LargeZipCodeTest.java
@@ -0,0 +1,29 @@
+package fr.efrei.playwright;
+
+import fr.efrei.playwright.utils.AddEmployeePageUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LargeZipCodeTest extends PlaywrightTeamSApplicationTests{
+
+    @Test
+    public void largeZipCodeTest() {
+
+        AddEmployeePageUtils addEmployeePageUtils = new AddEmployeePageUtils(page);
+
+        addEmployeePageUtils.goToAddEmployee()
+                .fillName("TestName")
+                .fillEmail("test@test.com")
+                .fillAddress("TestAddress")
+                .fillCity("TestCity")
+                .fillZipcode("738495789347589738947598734578934758")
+                .fillHiringDate("2021-01-02")
+                .fillJobTitle("TestJobTitle")
+                .clickAdd();
+
+        //check if error 500 is displayed
+        assertThat("Error 500 is displayed", !page.isVisible("text=Server Error (500)"));
+
+    }
+}
